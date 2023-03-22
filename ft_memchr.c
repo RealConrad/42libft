c7fd241dbb2d74e4af8c4906a22679789314af3a
cwenz <cwenz@student.42heilbronn.de>
2023-03-22 10:19:57 +0100

    added memchr.c file

---
+++ ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 08:58:41 by cwenz             #+#    #+#             */
+/*   Updated: 2023/03/22 10:19:22 by cwenz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*ptr_s;
+	size_t				i;
+
+	ptr_s = (const unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (*ptr_s == (unsigned char)c)
+			return ((void *)ptr_s);
+		i++;
+		ptr_s++;
+	}
+	return (NULL);
+}
