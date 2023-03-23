8e4b3f8f98ab82ec19170b0521c5ac195c92778d
cwenz <cwenz@student.42heilbronn.de>
2023-03-23 16:36:04 +0100

    added ft_substr.c file

--- ft_substr.c
+++ ft_substr.c
@@ -1,30 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/23 13:16:16 by cwenz             #+#    #+#             */
-/*   Updated: 2023/03/23 15:22:52 by cwenz            ###   ########.fr       */
+/*   Created: 2023/03/23 15:20:39 by cwenz             #+#    #+#             */
+/*   Updated: 2023/03/23 15:48:35 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*copy;
-	char	*str;
-	size_t	length;
+	char			*p;
+	char			*copy;
+	unsigned int	i;
+	unsigned int	j;
 
-	length = ft_strlen(s1);
-	copy = (char *)malloc(sizeof(char) * length + 1);
-	str = copy;
-	if (!copy)
+	i = start;
+	j = 0;
+	copy = (char *)malloc(sizeof(char) * (len + 1));
+	if (!copy || !s || start >= ft_strlen(s))
 		return (NULL);
-	while (*s1)
-		*str++ = *s1++;
-	*str = '\0';
-	return (copy);
+	p = copy;
+	while (s[i] && j < len)
+	{
+		p[j] = s[i];
+		j++;
+		i++;
+	}
+	p[j] = '\0';
+	return (p);
 }
