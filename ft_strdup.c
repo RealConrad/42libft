a6d0ed2afa529dc80d6617aa97bb739bed42cfa0
cwenz <cwenz@student.42heilbronn.de>
2023-03-23 15:21:38 +0100

    added ft_strdup.c file

---
+++ ft_strdup.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/23 13:16:16 by cwenz             #+#    #+#             */
+/*   Updated: 2023/03/23 15:11:38 by cwenz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*copy;
+	char	*str;
+	size_t	length;
+
+	length = ft_strlen(s1);
+	copy = (char *)malloc(sizeof(char) * length + 1);
+	str = copy;
+	if (!copy)
+		return (NULL);
+	while (*s1)
+		*str++ = *s1++;
+	*str = '\0';
+	return (copy);
+}
